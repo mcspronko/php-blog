33696e7efbd754ea429f06d1fdb98a4ba5270252
Max Pronko <max.pronko@gmail.com>
2020-12-06 00:35:52 +0200

    Added Post template and route - lesson 4

--- index.php
+++ index.php
@@ -28,4 +28,12 @@ $app->get('/about', function (Request $request, Response $response, $args) use (
     return $response;
 });
 
+$app->get('/{url_key}', function (Request $request, Response $response, $args) use ($view) {
+    $body = $view->render('post.twig', [
+        'url_key' => $args['url_key']
+    ]);
+    $response->getBody()->write($body);
+    return $response;
+});
+
 $app->run();
