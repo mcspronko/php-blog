284cd83eec18917fc3e3a07cc3440c4413178dad
Max Pronko <max.pronko@gmail.com>
2020-12-05 23:00:23 +0200

    Lesson 3 changes

--- index.php
+++ index.php
@@ -3,30 +3,28 @@
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
-use Twig\Loader\FilesystemLoader;
 use Twig\Environment;
+use Twig\Loader\FilesystemLoader;
 
 require __DIR__ . '/vendor/autoload.php';
-
-// Create app
-$app = AppFactory::create();
 
 $loader = new FilesystemLoader('templates');
 $view = new Environment($loader);
 
-$app->get('/', function (Request $request, Response $response, $args) use ($view) {
-    $content = $view->render('index.twig');
-    $response->getBody()->write($content);
+// Create app
+$app = AppFactory::create();
 
+$app->get('/', function (Request $request, Response $response, $args) use ($view) {
+    $body = $view->render('index.twig');
+    $response->getBody()->write($body);
     return $response;
 });
 
 $app->get('/about', function (Request $request, Response $response, $args) use ($view) {
-    $content = $view->render('about.twig', [
+    $body = $view->render('about.twig', [
         'name' => 'Max'
     ]);
-    $response->getBody()->write($content);
-
+    $response->getBody()->write($body);
     return $response;
 });
 
